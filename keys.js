024b486f3c5cefb8395d06b63c8161819be1e46e
Davey Wong <notwdjw@gmail.com>
2019-12-13 22:55:52 -0800

    Added package.json file and install necessary packages and dependencies

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+var clientID = '608aa51c3a3747f083dab3a6a17e1832'
+var clientSecert = '699f120840f74c0c974a571c71f26d8f'
---
+++ liri.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var fs = require('fs');
+var dotenv = require('dotenv');
+var moment = require('moment');
+var axios = require('axios')
